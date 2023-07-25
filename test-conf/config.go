735ea3a0121baf0433a09cbd82af2d3c1c14303a
jason <idea_wj@163.com>
2023-07-26 00:40:08 +0800

    更新测试

--- test-conf/config.go
+++ test-conf/config.go
@@ -7,37 +7,6 @@ import (
 	"log"
 )
 
-var (
-	_cfg *Settings
-)
-
-// Settings 兼容原先的配置结构
-type Settings struct {
-	Settings  Config `yaml:"settings"`
-	callbacks []func()
-}
-
-func (e *Settings) runCallback() {
-	for i := range e.callbacks {
-		e.callbacks[i]()
-	}
-}
-
-func (e *Settings) OnChange() {
-	e.init()
-	log.Println("!!! config change and reload")
-}
-
-func (e *Settings) Init() {
-	e.init()
-	log.Println("!!! config init")
-}
-
-func (e *Settings) init() {
-	e.Settings.multiDatabase()
-	e.runCallback()
-}
-
 // Config 配置集合
 type Config struct {
 	Application *Application          `yaml:"application"`
@@ -45,6 +14,28 @@ type Config struct {
 	Database    *Database             `yaml:"database"`
 	Databases   *map[string]*Database `yaml:"databases"`
 	Gen         *Gen                  `yaml:"gen"`
+	callbacks   []func()
+}
+
+func (e *Config) runCallback() {
+	for i := range e.callbacks {
+		e.callbacks[i]()
+	}
+}
+
+func (e *Config) OnChange() {
+	e.init()
+	log.Println("!!! config change and reload")
+}
+
+func (e *Config) Init() {
+	e.init()
+	log.Println("!!! config init")
+}
+
+func (e *Config) init() {
+	e.multiDatabase()
+	e.runCallback()
 }
 
 // 多db改造
@@ -60,15 +51,13 @@ func (e *Config) multiDatabase() {
 // Setup 载入配置文件
 func Setup(s source.Source,
 	fs ...func()) {
-	_cfg = &Settings{
-		Settings: Config{
-			Application: ApplicationConfig,
-			Auth:        AuthConfig,
-			Database:    DatabaseConfig,
-			Databases:   &DatabasesConfig,
-			Gen:         GenConfig,
-		},
-		callbacks: fs,
+	_cfg := &Config{
+		Application: ApplicationConfig,
+		Auth:        AuthConfig,
+		Database:    DatabaseConfig,
+		Databases:   &DatabasesConfig,
+		Gen:         GenConfig,
+		callbacks:   fs,
 	}
 	var err error
 	loadconfig.DefaultConfig, err = loadconfig.NewConfig(
