d4e31126247cd6a5eddf84d4d2b8406c09c1d5c9
Jason <none>
2023-10-25 14:43:00 +0800

    结构简化

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # load-config
-使用方式课擦看test-conf目录中的`conf_test.go`
+使用方式可查看test-conf目录中的`conf_test.go`
--- encoder/yaml/yaml.go
+++ encoder/yaml/yaml.go
@@ -2,17 +2,17 @@ package yaml
 
 import (
 	"github.com/bitxx/load-config/encoder"
-	"github.com/ghodss/yaml"
+	"github.com/bitxx/load-config/encoder/yaml/parser"
 )
 
 type yamlEncoder struct{}
 
 func (y yamlEncoder) Encode(v interface{}) ([]byte, error) {
-	return yaml.Marshal(v)
+	return parser.Marshal(v)
 }
 
 func (y yamlEncoder) Decode(d []byte, v interface{}) error {
-	return yaml.Unmarshal(d, v)
+	return parser.Unmarshal(d, v)
 }
 
 func (y yamlEncoder) String() string {
